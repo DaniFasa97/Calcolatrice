ca6238ebeedef193b2a224e0c228e7b1f1e6ac05
danifasa97 <danifasa97@gmail.com>
2021-10-01 12:17:01 +0200

    add Esercizio41

---
+++ app/src/main/java/com/v1bottoni/myapplication/Esercizio41.kt
@@ -0,0 +1,10 @@
+package com.v1bottoni.myapplication
+
+class Esercizio41()
+
+    fun main() {
+        val numbers = mutableListOf("one", "two", "three")
+        numbers.add("five")
+        println(numbers)
+    }
+
