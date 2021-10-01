d43ada8326755a34a3069fdfeb2ba48ec481f215
danifasa97 <danifasa97@gmail.com>
2021-10-01 14:40:33 +0200

    add U45

---
+++ app/src/main/java/com/v1bottoni/myapplication/Esercizio45.kt
@@ -0,0 +1,9 @@
+package com.v1bottoni.myapplication
+
+fun main() {
+    val numbers = arrayListOf("one", "two", "three","three")
+    val list = numbers.toSet()
+    for(s in list) println(s)
+
+
+}
