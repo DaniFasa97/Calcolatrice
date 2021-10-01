73c22e95400ccb9e259ae6236f2127a03aefbabf
danifasa97 <danifasa97@gmail.com>
2021-10-01 14:31:56 +0200

    add  U44

---
+++ app/src/main/java/com/v1bottoni/myapplication/Esercizio44.kt
@@ -0,0 +1,21 @@
+package com.v1bottoni.myapplication
+
+fun main() {
+    val numbers = hashMapOf("one" to 1, "two" to 2, "three" to 3)
+    for(s in numbers) println(numbers)
+    fun hashMapOf(pairs: HashMap<String, Int>) {
+
+    }
+
+    
+}
+
+
+
+
+
+
+
+
+
+
