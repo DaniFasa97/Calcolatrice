2c339a4e703d43fa9284f72fe09870a84de9c5f5
danifasa97 <danifasa97@gmail.com>
2021-10-01 12:51:11 +0200

    add u43

---
+++ app/src/main/java/com/v1bottoni/myapplication/Esercizio43.kt
@@ -0,0 +1,9 @@
+package com.v1bottoni.myapplication
+
+    fun main() {
+        val numbers = arrayListOf("one", "two", "three")
+        val list = numbers.toMutableList()
+        for(s in list) println(s)
+
+
+}
