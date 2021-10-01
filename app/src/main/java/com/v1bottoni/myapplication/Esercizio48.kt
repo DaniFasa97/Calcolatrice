b1be380f02caad74cdcec53a1c2de31d036f79d2
danifasa97 <danifasa97@gmail.com>
2021-10-01 16:30:30 +0200

    add u48

--- app/src/main/java/com/v1bottoni/myapplication/Esercizio48.kt
+++ app/src/main/java/com/v1bottoni/myapplication/Esercizio48.kt
@@ -1,4 +1,12 @@
 package com.v1bottoni.myapplication
 
-class Esercizio48 {
+import android.media.session.PlaybackState
+import kotlin.jvm.internal.Ref
+
+fun main(){
+    val arrayList = ArrayList<String>()
+   val customProperty: String
+   println()
+
+
 }
